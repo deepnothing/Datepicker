e4a84f7cc03d600e0ae847217e61d5f15a711507
deepnothing <65370281+deepnothing@users.noreply.github.com>
2021-04-13 19:57:33 -0400

    add: initialCommit

--- src/Datepicker.js
+++ src/Datepicker.js
@@ -12,21 +12,23 @@ export default function Datepicker() {
   let daysinmontharray = Array.apply(null, { length: daysInMonth + 1 }).map(Number.call, Number);
   daysinmontharray.shift();
 
-  let Calculate = Math.round((new Date(mydate.month + '/' + mydate.day + '/' + mydate.year) - new Date(todaystring)) / (1000 * 3600 * 24))
+  let Calculate = Math.round((new Date(mydate.month + '/' + mydate.day + '/' + mydate.year) - new Date(todaystring)) / (1000 * 3600 * 24));
+
+  let isCalcNegative = Math.sign(Calculate) === 1 || Calculate === 0; let isCalcOne = Math.sign(Calculate) === 1 || Calculate === -1
 
   return (
     <div>
       <div className="wrapper">
         <div className="datewrapper">
 
-          <div><label for="month">Month:</label>
+          <div><label>Month</label>
             <select className="p" id="month" value={mydate.month} onChange={(e) => { localStorage.setItem('mydate', JSON.stringify({ ...mydate, 'month': e.target.value })); setMydate({ ...mydate, 'month': e.target.value }) }} >
               {monthlist.map((item, i) =>
                 <option key={i} value={i + 1}>{item}</option>
               )}
             </select></div>
 
-          <div><label for="day">Day:</label>
+          <div><label style={{ marginLeft: '-15px' }}>Day</label>
             <select className="p" id="day" value={mydate.day} onChange={(e) => { localStorage.setItem('mydate', JSON.stringify({ ...mydate, 'day': e.target.value })); setMydate({ ...mydate, 'day': e.target.value }) }}>
               {
                 daysinmontharray.map((item, i) =>
@@ -35,12 +37,12 @@ export default function Datepicker() {
               }
             </select></div>
 
-          <div><label for="year">Year:</label>
-            <input className="p" id="year" type="number" min="0" defaultValue={mydate.year} onInput={(e) => { localStorage.setItem('mydate', JSON.stringify({ ...mydate, 'year': Math.abs(e.target.value) })); setMydate({ ...mydate, 'year': Math.abs(e.target.value) }) }}></input>
+          <div id="yr"><label style={{ marginLeft: '-20px' }}>Year</label>
+            <input className="p" id="year" type="number" defaultValue={mydate.year} onInput={(e) => { if (e.target.value < 275759) { localStorage.setItem('mydate', JSON.stringify({ ...mydate, 'year': Math.abs(e.target.value) })); setMydate({ ...mydate, 'year': Math.abs(e.target.value) }) } }}></input>
           </div></div>
 
         <div className="output">
-          {mydate.month + '/' + mydate.day + '/' + mydate.year} {(Math.sign(Calculate) === 1 || Calculate === 0) ? <span>is</span> : <span>was</span>} {Math.abs(Calculate)} {(Calculate === 1 || Calculate === -1) ? <span>day</span> : <span>days</span>} {(Math.sign(Calculate) === 1 || Calculate === 0) ? <span>from today</span> : <span>ago</span>}</div>
+          {mydate.month + '/' + mydate.day + '/' + mydate.year} {(isCalcNegative) ? <span>is</span> : <span>was</span>} {Math.abs(Calculate)} {(isCalcOne) ? <span>day</span> : <span>days</span>} {(isCalcNegative) ? <span>from today</span> : <span>ago</span>}</div>
       </div>
     </div >
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Datepicker from './App';
+import Datepicker from './Datepicker';
 
 
 ReactDOM.render(
